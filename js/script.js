246807ce2df436d01206962529864924c57f8b41
YunusBD <yunus@efleetpass.com.au>
2024-01-03 02:12:49 +0600

    some changes is added

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(window).scroll(function() {    
     var scroll = $(window).scrollTop();
-    if (scroll >= 200) {
+    if (scroll >= 50) {
         $(".main-navbar").addClass("navbar_setbg");
     }else{
         $(".main-navbar").removeClass("navbar_setbg");
